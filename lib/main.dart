44fd6deff5bf6009076bab3457739b4840b90f10
ROCKY <riazulrocky@gmail.com>
2024-07-14 17:41:13 +0600

    front page

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Front Page',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
